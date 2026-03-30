f0413fb0c417fb56f9e894d6bf7bb4ab62fd8275
Tarek Allam Jr <t.allam.jr@gmail.com>
2026-03-30 12:51:59 +0100

    refactor: remove dead source files that were never imported or compiled

--- src/gst_plugins/mod.rs
+++ src/gst_plugins/mod.rs
@@ -3,13 +3,8 @@
 use gst::glib;
 use gstreamer as gst;
 
-pub mod detection_demo;
-pub mod file_output_demo;
 pub mod pupinference;
 pub mod pupoverlay;
-pub mod simple_demo;
-pub mod simple_visual_demo;
-pub mod visual_demo;
 
 // Plugin registration following gstreamer-rs patterns
 gst::plugin_define!(
